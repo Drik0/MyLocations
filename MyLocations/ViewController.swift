2659b6eb970766ae1f050785557e630666251075
Gerson Costa <gerson.lcc@hotmail.com>
2019-06-10 13:44:11 +0100

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Gerson Costa on 10/06/2019.
+//  Copyright © 2019 Gerson Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
