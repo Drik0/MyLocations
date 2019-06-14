7b4737514e60e03f6e757edb0ec6184218581514
Gerson Costa <gerson.lcc@hotmail.com>
2019-06-14 13:12:38 +0100

    HUD Image and Text

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -24,6 +24,7 @@ class HudView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
+        // HUD Rectangle
         let boxWidth: CGFloat = 96
         let boxHeight: CGFloat = 96
         
@@ -37,5 +38,25 @@ class HudView: UIView {
         
         UIColor(white: 0.3, alpha: 0.8).setFill()
         roundedRect.fill()
+        
+        // HUD Checkmark Image
+        if let image = UIImage(named: "Checkmark") {
+            let imagePoint = CGPoint(
+                x: center.x - round(image.size.width / 2),
+                y: center.y - round(image.size.height / 2) - boxHeight / 8)
+            image.draw(at: imagePoint)
+        }
+        
+        // HUD Text
+        let attribs = [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor : UIColor.white]
+        
+        let textSize = text.size(withAttributes: attribs)
+        
+        let textPoint = CGPoint(
+            x: center.x - round(textSize.width / 2),
+            y: center.y - round(textSize.height / 2) + boxHeight / 4)
+        text.draw(at: textPoint, withAttributes: attribs)
     }
 }
